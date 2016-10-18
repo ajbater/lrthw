69007f0420d94dbd64971a20625d6fb5190d6d7a
Amanda Bater <amanda22@hotmail.co.uk>
2016-10-18 22:21:53 +0100

    Exercise 3

---
+++ chap01/ex3.rb
@@ -0,0 +1,38 @@
+#prints to the screen
+puts "I will now count my chickens:"
+
+#inserting a calculation to find the number of hens into a string
+puts "Hens #{25 + 30 / 6}"
+#inserting a calculation to find the number of roosters into a string
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+#prints to the string
+puts "Now I will count the eggs:"
+
+#prints a calculation for number of eggs
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#print to the screen. These are not integers, they are in a string.
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#works out if 3 + 2 (5) is less than 5 - 7 (-2). It is not, so this returns false.
+#'false' will then be printed to the screen.
+puts 3 + 2 < 5 - 7
+
+#prints a string asking the answer to 3 + 2. The calculation is inserted into the string.
+puts "What is 3 + 2? #{3 + 2}"
+#prints a string asking the answer to 5 - 7. The calculation is inserted into the string.
+puts "What is 5 - 7? #{5 - 7}"
+
+#prints a string to the screen.
+puts "Oh, that's why it's false."
+
+#prints a string to the screen.
+puts "How about some more."
+
+#asks which of two numbers is greater. The calculation is inserted at the end of the string.
+puts "Is it greater? #{5 > -2}"
+#asks if one number is greater or equal to the other. The calculation is inserted at the end of the string.
+puts "Is it greater or equal? #{5 >= -2}"
+#asks if one number is less than or equal to the other. The calculation is inserted at the end of the string.
+puts "Is it less or equal? #{5 <= -2}"
