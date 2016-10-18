573445d5822f4d60c9a504e83af4a09b3a16567d
Amanda Bater <amanda22@hotmail.co.uk>
2016-10-18 22:47:30 +0100

    Updating Exercise 4 to include my comments for the Study Drills

--- chap01/ex4.rb
+++ chap01/ex4.rb
@@ -1,10 +1,18 @@
+# variable for number of cars
 cars = 100
+# variable for number of spaces in a car
 space_in_a_car = 4.0
+# variable for how many drivers
 drivers = 30
+# variable for how many passengers
 passengers = 90
+# variable for how many cars are not being driven
 cars_not_driven = cars - drivers
+# variable for how many cars are being driven
 cars_driven = drivers
+# variable for for how many carpools can be offered
 carpool_capacity = cars_driven * space_in_a_car
+# variable for the avergage number of passengers in each car
 average_passengers_per_car = passengers / cars_driven
 
 
@@ -14,3 +22,6 @@ puts "There will be #{cars_not_driven} empty cars today."
 puts "We can transport #{carpool_capacity} people today."
 puts "We have #{passengers} to carpool today."
 puts "We need to put about #{average_passengers_per_car} in each car."
+
+# The error the programmer first got when writing this program was on line 14.
+# The variable has either been written wrong or nothing has been assigned to it.
