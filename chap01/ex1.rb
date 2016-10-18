1710d7929e4f09d738a02699fbc931be2686e955
Amanda Bater <amanda22@hotmail.co.uk>
2016-10-18 21:56:49 +0100

    Exercise 1

---
+++ chap01/ex1.rb
@@ -0,0 +1,8 @@
+#puts "Hello World!"
+#puts "Hello Again"
+#puts "I like typing this."
+#puts "This is fun,"
+#puts "Yay! Printing!"
+#puts "I'd much rather you 'not'."
+#puts 'I "said" do not touch this.'
+puts "I am making my script print another line, because this is the study drill! I have commented out all of the other lines so that only this one prints, as that is also a study drill."
