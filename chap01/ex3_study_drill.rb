ead75e14b2c9ff0bb8bb2a37f5b04ed7be3f5c67
Amanda Bater <amanda22@hotmail.co.uk>
2016-10-18 22:28:03 +0100

    Exercise 3 Study Drill

---
+++ chap01/ex3_study_drill.rb
@@ -0,0 +1,4 @@
+# The study drill asks you to create a new .rb file with a calculation.
+# I will calculate how many seconds are in a year.
+
+puts "There are #{(((60 * 60) * 24) * 365)} seconds in a year."
