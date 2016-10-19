07d5c46babc4ccb6e0abef3fd397176711219381
Amanda Bater <amanda22@hotmail.co.uk>
2016-10-19 20:43:41 +0100

    Exercise 13 and Study Drills

---
+++ chap01/ex13_study_drill1.rb
@@ -0,0 +1,4 @@
+person1, person2 = ARGV
+
+puts "Person 1 is #{person1}"
+puts "Person 2 is #{person2}"
---
+++ chap01/ex13_study_drill2.rb
@@ -0,0 +1,7 @@
+person1, person2, person3, person4, person5 = ARGV
+
+puts "Person 1 is #{person1}"
+puts "Person 2 is #{person2}"
+puts "Person 3 is #{person3}"
+puts "Person 4 is #{person4}"
+puts "Person 5 is #{person5}"
