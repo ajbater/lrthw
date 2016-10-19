b926def272448cec8211ff56bfb88f8b2a2fe1d8
Amanda Bater <amanda22@hotmail.co.uk>
2016-10-19 08:01:40 +0100

    Updating Exercise 6 to include Study Drills

--- chap01/ex6.rb
+++ chap01/ex6.rb
@@ -1,21 +1,36 @@
+# assigns a variable for types of people
 types_of_people = 10
+# assigns a variable 'x' with a string including another variable
 x = "There are #{types_of_people} types of people."
+# assigns a variable with a string
 binary = "binary"
+# assigns a variable with a string
 do_not = "don't"
+# assigns a variable with a string containing two more variables
 y = "Those who know #{binary} and those who #{do_not}."
 
+# prints the contents of the variable 'x' to the screen
 puts x
+# prints the contents of the variable 'y' to the screen
 puts y
 
+# prints a new string that also includes the contents of the variable 'x'
 puts "I said: #{x}."
+# prints a new string that also includes the contents of the variable 'y'
 puts "I also said: '#{y}'."
 
+# assigns a false value to a variable
 hilarious = false
+# assigns a variable with a string containing another variable
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
+# prints the contents of the 'joke_evaluation' variable to the screen
 puts joke_evaluation
 
+# assigns a string to a variable
 w = "This is the left side of..."
+# assigns a string to a variable
 e = "a string with a right side."
 
+# prints the contents of the variable 'w' combined with the variable 'e'
 puts w + e
