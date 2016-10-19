58c1f65534a794638f2d16c392f42fc10c6c814e
Amanda Bater <amanda22@hotmail.co.uk>
2016-10-19 21:16:09 +0100

    Updating Exercise 14 to include the changes suggested in Study Drills

--- chap01/ex14.rb
+++ chap01/ex14.rb
@@ -1,13 +1,13 @@
-user_name = ARGV.first
-prompt = '> '
+user_name1, user_name2 = ARGV
+prompt = '... '
 
-puts "Hi #{user_name}."
+puts "Hi #{user_name1}."
 puts "I'd like to ask you a few questions."
-puts "Do you like me #{user_name}? "
+puts "Do you like me #{user_name1}? "
 puts prompt
 likes = $stdin.gets.chomp
 
-puts "Where do you live #{user_name}? "
+puts "Where do you live #{user_name1}? "
 puts prompt
 lives = $stdin.gets.chomp
 
@@ -19,3 +19,23 @@ Alright, so you said #{likes} about liking me.
 You live in #{lives}. Not sure where that is.
 And you have a #{computer} computer. Nice.
 """
+
+puts "How about you, #{user_name2}? Do you like pie? "
+puts prompt
+pie = $stdin.gets.chomp
+
+puts "Do you like cats? "
+puts prompt
+cats = $stdin.gets.chomp
+
+if pie.downcase == "yes" && cats.downcase == "yes"
+  puts """
+  Well, that's all I need to know about you, #{user_name2}.
+  We like all the same things and will be best of friends.
+  """
+else puts """
+  I'm sorry, #{user_name2}.
+  I see that we do not have enough in common.
+  We will never be great friends.
+  """
+end
