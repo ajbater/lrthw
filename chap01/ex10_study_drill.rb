e825ec6b1a73658fd717f1a2f1d362f1d976c6c9
Amanda Bater <amanda22@hotmail.co.uk>
2016-10-19 19:54:16 +0100

    Study Drill for Exercise 10

---
+++ chap01/ex10_study_drill.rb
@@ -0,0 +1,20 @@
+puts '''
+I am trying to use three single-quote marks
+instead of three double-quotes marks
+to see what the difference is
+and why I might need to do that instead.
+  '''
+
+  puts "I will try to use a\vvertical tab."
+
+  puts "I will see what\rdoes"
+
+  puts 'It\'s fun to make a single-quote work with an escape sequence.'
+
+  puts "It's also \"fun\" to use an escape sequence with double-quotes."
+
+  puts "I guess I would use this if I wanted to include a hex value like \uA10F"
+
+  puts "What will this\ado?"
+
+  puts "My\vfavourite\vescape\vsequence\vis\v\"vertical tab\"\vbecause\vthings\vlook\vlike\vthey\vare\vfalling.\v.\v.\v.\v....."
