863acbbc6b39587695cba64417abebe2a7e44b8e
Amanda Bater <amanda22@hotmail.co.uk>
2016-10-19 08:11:42 +0100

    Exercise 7 - including Study Drills

---
+++ chap01/ex7.rb
@@ -0,0 +1,27 @@
+# prints to the screen
+puts "Mary had a little lamb."
+# prints a string to the screen, inserts the string 'snow' to the string
+puts "Its fleece was white as #{'snow'}."
+# prints to the screen
+puts "And everywhere that Mary went."
+# puts "." 10 times
+puts "." * 10
+
+# end1..end12 all of these are variables with strings assigned to them
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# prints the contents of these variables to the screen one after another using 'print'
+print end1 + end2 + end3 + end4 + end5 + end6
+# prints the contents of these variables to the screen one after another using 'puts'
+puts end7 + end8 + end9 + end10 + end11 + end12
