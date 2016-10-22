081bede4172e24945be4462ed1474ca16a155a06
Amanda Bater <amanda22@hotmail.co.uk>
2016-10-22 14:07:39 +0100

    Exercise 33 Study Drill

--- chap01/ex33_study_drill.rb
+++ chap01/ex33_study_drill.rb
@@ -18,3 +18,22 @@ end
 while_loop(20, 3)
 while_loop(3, 1)
 while_loop(7, 2)
+
+def for_loop(i, range)
+  numbers = []
+
+  for i in (i..range)
+    puts "At the top i is #{i}"
+    numbers.push(i)
+
+    puts "Numbers now: ", numbers
+    puts "At the bottom i is #{i}"
+  end
+
+  puts "The numbers: "
+
+  numbers.each { |num| puts num }
+
+end
+
+for_loop(0, 10)
