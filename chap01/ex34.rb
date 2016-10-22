0d2f8b5eb489df662efdb612d1b07fcd0497d05f
Amanda Bater <amanda22@hotmail.co.uk>
2016-10-22 14:43:26 +0100

    Exercise 34

---
+++ chap01/ex34.rb
@@ -0,0 +1,9 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaoo', 'whale', 'platypus']
+
+puts "The animal at 1 is the 2nd animal and is a #{animals[1]}"
+puts "The animal at 2 is the 3rd animal and is a #{animals[2]}"
+puts "The animal at 0 is the 1st animal and is a #{animals[0]}"
+puts "The animal at 3 is the 4th animal and is a #{animals[3]}"
+puts "The animal at 4 is the 5th animal and is a #{animals[4]}"
+puts "The animal at 2 is the 3rd animal and is a #{animals[2]}"
+puts "The animal at 4 is the 5th animal and is a #{animals[4]}"
