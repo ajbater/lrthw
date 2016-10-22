9fece3b152825d6d357e369a53c6751b4041bc1e
Amanda Bater <amanda22@hotmail.co.uk>
2016-10-22 14:50:20 +0100

    Exercise 34 updated to include Study Drills

--- chap01/ex34.rb
+++ chap01/ex34.rb
@@ -7,3 +7,20 @@ puts "The animal at 3 is the 4th animal and is a #{animals[3]}"
 puts "The animal at 4 is the 5th animal and is a #{animals[4]}"
 puts "The animal at 2 is the 3rd animal and is a #{animals[2]}"
 puts "The animal at 4 is the 5th animal and is a #{animals[4]}"
+
+# Study Drill...more arrays
+
+fruits = ['apple', 'banana', 'melon', 'kiwi', 'pineapple']
+
+puts "The fruit at 0 is the 1st fruit and is a #{fruits[0]}"
+puts "The fruit at 1 is the 2nd fruit and is a #{fruits[1]}"
+puts "The fruit at 2 is the 3rd fruit and is a #{fruits[2]}"
+puts "The fruit at 3 is the 4th fruit and is a #{fruits[3]}"
+puts "The fruit at 4 is the 5th fruit and is a #{fruits[4]}"
+
+vegetables = ['potato', 'broccoli', 'turnip', 'carrot']
+
+puts "The vegetable at 0 is the 1st vegetable and is a #{vegetables[0]}"
+puts "The vegetable at 1 is the 2nd vegetable and is a #{vegetables[1]}"
+puts "The vegetable at 2 is the 3rd vegetable and is a #{vegetables[2]}"
+puts "The vegetable at 3 is the 4th vegetable and is a #{vegetables[3]}"
