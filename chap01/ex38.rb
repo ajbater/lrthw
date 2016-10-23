c9a825a939f54e21f2dcf010566e8e93edcce949
Amanda Bater <amanda22@hotmail.co.uk>
2016-10-23 10:00:41 +0100

    Exercise 38

---
+++ chap01/ex38.rb
@@ -0,0 +1,29 @@
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+
+puts "Wait, there are not 10 things in that list. Let's fix that."
+
+# split(ten_things, ' ')
+stuff = ten_things.split(' ')
+more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
+
+while stuff.length != 10
+  # pop(more_stuff)
+  next_one = more_stuff.pop
+  puts "Adding: #{next_one}"
+  # push(more_stuff, next_one)
+  stuff.push(next_one)
+  puts "There are #{stuff.length} items now."
+end
+
+puts "There we go: #{stuff}"
+
+puts "Let's do some things with stuff."
+
+puts stuff[1]
+puts stuff[-1]
+# pop(stuff)
+puts stuff.pop()
+# join(stuff, ' ')
+puts stuff.join(' ')
+# join(stuff[3..5], '#')
+puts stuff[3..5].join("#")
